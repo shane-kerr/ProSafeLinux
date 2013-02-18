302acd521dc3249de91cbe0874b53d8cd52705c3
Shane Kerr <shane@time-travellers.org>
2013-02-18 23:20:04 +0100

    Add script to run unit tests with all available Python interpreters.

---
+++ run_tests.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+PYTHON_VERSIONS="python2.7 python3.1 python3.2 pypy"
+TEST_SCRIPTS="testNSDP.py"
+
+for PYTHON_VERSION in $PYTHON_VERSIONS; do
+    PYTHON=`which $PYTHON_VERSION`
+    if [ $? -eq 0 ]; then
+        echo Testing with $PYTHON_VERSION
+        for TEST_SCRIPT in $TEST_SCRIPTS; do
+            $PYTHON $TEST_SCRIPT
+            if [ $? -ne 0 ]; then
+                exit $?
+            fi
+        done
+    else
+        echo No executable found for $PYTHON_VERSION
+    fi
+done
+
